580eb073a3490807a24bf7f836540db865415bf6
Syy <shirakawa.yy@gmail.com>
2018-09-19 00:39:46 +0900

    Add GameScene Controller

---
+++ Assets/Scripts/Scene/GameScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Manager;
+using UnityEngine;
+
+public class GameScene : MonoBehaviour {
+
+    GameManager _gameManager;
+	// Use this for initialization
+	void Awake () {
+        _gameManager = new GameManager();
+        _gameManager.Init();
+	}    
+}
