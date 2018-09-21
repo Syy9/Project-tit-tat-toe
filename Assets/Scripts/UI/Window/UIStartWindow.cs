dc3b3f57434ea3b97ffbf35477e678ef5e774ab6
Syy <shirakawa.yy@gmail.com>
2018-09-21 23:40:33 +0900

    Add UIStartWindow

---
+++ Assets/Scripts/UI/Window/UIStartWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UI.Window;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStartWindow : UIWindow {
+    public Action OnClick;
+    [SerializeField] Button touchButton;
+
+    void Awake()
+    {
+        touchButton.onClick.AddListener(() => {
+            OnClick.Call();
+        });
+    }
+}
