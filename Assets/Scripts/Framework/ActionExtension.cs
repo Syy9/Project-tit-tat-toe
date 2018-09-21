6c23fd5b35f864345d1f6d4a07cb0afddad2f61d
Syy <shirakawa.yy@gmail.com>
2018-09-21 23:40:25 +0900

    Add ActionExtention

---
+++ Assets/Scripts/Framework/ActionExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class ActionExtention
+{
+    public static void Call(this Action self)
+    {
+        if(self != null)
+        {
+            self();
+        }
+    }
+
+    public static void Call<T1>(this Action<T1> self, T1 arg1)
+    {
+        if(self != null)
+        {
+            self(arg1);
+        }
+    }
+
+    public static void Call<T1,T2>(this Action<T1,T2> self, T1 arg1, T2 arg2)
+    {
+        if (self != null)
+        {
+            self(arg1, arg2);
+        }
+    }
+}
