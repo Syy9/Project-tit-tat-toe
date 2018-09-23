32aafe0d79219495951102d4b40b572f3e9a1b54
Syy <shirakawa.yy@gmail.com>
2018-09-23 23:20:56 +0900

    Add SceneController

---
+++ Assets/Scripts/Scene/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(ExecutionOrder.LEVEL_SCENE)]
+public class SceneController : MonoBehaviour {
+    void Awake()
+    {
+        OnAwake();
+    }
+
+    protected virtual void OnAwake() { }
+}
