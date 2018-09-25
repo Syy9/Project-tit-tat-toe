50773dd84f21d9008b8c1a380d9082800e1fe34f
Syy <shirakawa.yy@gmail.com>
2018-09-25 22:56:53 +0900

    Add PlayerType

---
+++ Assets/Scripts/Logic/InGame/Player/PlayerType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public enum PlayerType
+{
+    None,
+    Player1,
+    Player2,
+}
+
+public static class PlayerTypeExtentions
+{
+    public static Color GetColor(this PlayerType self)
+    {
+        switch (self)
+        {
+            case PlayerType.None:
+                return Color.white;
+            case PlayerType.Player1:
+                return Color.blue;
+            case PlayerType.Player2:
+                return Color.red;
+            default:
+                throw new NotImplementedException("Not Implemented GetColor. type=" + self);
+        }
+    }
+
+    public static PlayerType GetNextPlayer(this PlayerType self)
+    {
+        switch (self)
+        {
+            case PlayerType.Player1:
+                return PlayerType.Player2;
+            case PlayerType.Player2:
+                return PlayerType.Player1;
+            default:
+                throw new NotImplementedException("Not Implemented GetColor. type=" + self);
+        }
+    }
+}
