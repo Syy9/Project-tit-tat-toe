88598d7b47f98a173bced4ef9f3abda228bb5932
Syy <shirakawa.yy@gmail.com>
2018-09-27 23:32:31 +0900

    Add UIResultWindow

---
+++ Assets/Scripts/UI/Window/UIResultWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UI.Window;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIResultWindow : UIWindow
+{
+    public Action OnCloseEvent;
+
+    [SerializeField] TextMeshProUGUI winnerText;
+    [SerializeField] Button closeButton;
+
+    void Awake()
+    {
+        closeButton.onClick.AddListener(() => {
+            OnCloseEvent.Call();
+        });
+    }
+
+    public void Init(PlayerType winner)
+    {
+        winnerText.text = winner.ToString();
+    }
+}
